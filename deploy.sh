3ea9add0d62221196c8434d73616f7287c8ff2ac
chankoo <ckbaek1125@gmail.com>
2023-12-29 10:38:43 +0900

    Rebuild site Fri Dec 29 10:38:43 KST 2023

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,26 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Replace Obsidian links with Hugo links
-cd content/posts
+cd content/pages
+for file in *.md
+do
+  tmp_file1="$file.tmp1"
+  tmp_file2="$file.tmp2"
+
+  # 너비가 지정된 이미지 링크 처리
+  sed 's/!\[\[\(.*\)|\(.*\)\]\]/{{< image src="\/images\/\1" width="\2" >}}/g' "$file" > "$tmp_file1"
+  
+  # 너비가 지정되지 않은 이미지 링크 처리
+  sed 's/!\[\[\(.*\)\]\]/{{< image src="\/images\/\1" >}}/g' "$tmp_file1" > "$tmp_file2"
+
+  # 일반 텍스트 링크 처리
+  sed -r 's/\[\[(.*)\]\]/[\1]({{< ref "\/posts\/\1.md" >}})/g' "$tmp_file2" > "$file"
+
+  # 임시 파일 삭제
+  rm "$tmp_file1" "$tmp_file2"
+done
+
+cd ../posts
 
 for file in *.md
 do
